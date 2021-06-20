5222bc1dae6de9c3bdf170e34ab3a4cda0642778
Jim Hill <jim@tightenupthe.tech>
2021-06-21 00:23:39 +0100

    feat: optional checkout parameter

--- scripts/fish/checkout.fish
+++ scripts/fish/checkout.fish
@@ -1,6 +1,11 @@
 function checkout
-    set -l selected_branch (
-        git branch --sort=-committerdate | sed "s/.* //" | fzf --preview="git log --color {}"
-    )
-    command git checkout $selected_branch
+    # if no argument, fuzzy find a branch
+    if [ -z "$argv" ];
+        set -l selected_branch (
+            git branch --sort=-committerdate | sed "s/.* //" | fzf --preview="git log --color {}"
+        )
+        command git checkout $selected_branch
+    else
+        command git checkout $argv[1]
+    end
 end
