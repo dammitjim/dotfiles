2a3b881d563a9bf1b9fbc9c5cee9815fc58e3211
Jim Hill <jim@tightenupthe.tech>
2021-06-22 00:47:01 +0100

    feat: pg restore functionality, abstract yq parsing into separate function

---
+++ scripts/fish/pg_docker_parse.fish
@@ -0,0 +1,15 @@
+function pg_docker_parse
+    set -l database (
+        yq e '.services[] | select(.image == "postgres*").environment | .POSTGRES_DB // "postgres"' docker-compose.yml
+    )
+    set -l port (
+        yq e '.services[] | select(.image == "postgres*").ports | .[] | select(. == "*5432") | split(":") | .[0]' docker-compose.yml
+    )
+    set -l password (
+        yq e '.services[] | select(.image == "postgres*").environment.POSTGRES_PASSWORD' docker-compose.yml
+    )
+    set -l user (
+        yq e '.services[] | select(.image == "postgres*").environment | .POSTGRES_USER // "postgres"' docker-compose.yml
+    )
+    echo $database\n$port\n$password\n$user
+end
--- scripts/fish/pg_restore_docker.fish
+++ scripts/fish/pg_restore_docker.fish
@@ -1,12 +1,12 @@
-function pg_dump_docker
+function pg_restore_docker
     if count $argv > /dev/null
         set -l keys (pg_docker_parse)
         set -l database $keys[1]
         set -l port $keys[2]
         set -l password $keys[3]
         set -l user $keys[4]
-        echo "Dumping $database from 127.0.0.1:$port to file $argv"
-        PGPASSWORD="$password" pg_dump $database -h 127.0.0.1 -p $port -U $user -Fc --file=$argv
+        echo "Restoring $database to 127.0.0.1:$port from file $argv"
+        PGPASSWORD="$password" pg_restore -d $database -h 127.0.0.1 -p $port -U $user -Fc $argv
     else
         echo "Provide a filename as the first argument"
     end
